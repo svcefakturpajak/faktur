ee7f5c843a9f167069dcb6fdf9deb8c947a1c98b
svcefakturpajak <113603360+svcefakturpajak@users.noreply.github.com>
2022-09-15 05:02:40 -0700

    Add files via upload

---
+++ svcefakturpajak.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en-US" data-website-id="1" data-main-object="website.page(5,)" data-oe-company-name="svcefakturpajak">
+    <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="generator" content="Odoo"/>
+        <meta name="robots" content="noindex"/>
+            
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="svc.efaktur.pajak.go.id/validasi/faktur/808823744429000/0061946519104/3031300D0609608648016503040201050004202D64BDCDD533F98889C5D002604 | svc.efaktur.pajak.co.id"/>
+        <meta property="og:site_name" content="svcefakturpajak"/>
+        <meta property="og:url" content="https://svcefakturpajak.odoo.com/svc-efaktur-pajak-go-id/validasi/faktur/808823744429000/0061946519104/3031300d0609608648016503040201050004202d64bdcdd533f98889c5d002604"/>
+        <meta property="og:image" content="https://svcefakturpajak.odoo.com/web/image/website/1/logo?unique=be92d55"/>
+            
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:title" content="svc.efaktur.pajak.go.id/validasi/faktur/808823744429000/0061946519104/3031300D0609608648016503040201050004202D64BDCDD533F98889C5D002604 | svc.efaktur.pajak.co.id"/>
+        <meta name="twitter:image" content="https://svcefakturpajak.odoo.com/web/image/website/1/logo/300x300?unique=be92d55"/>
+
+        <link rel="canonical" href="https://svc.efaktur.pajak.go.id/svc-efaktur-pajak-go-id/validasi/faktur/808823744429000/0061946519104/3031300d0609608648016503040201050004202d64bdcdd533f98889c5d002604"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin=""/>
+        <title> svc.efaktur.pajak.go.id/validasi/faktur/808823744429000/0061946519104/3031300D0609608648016503040201050004202D64BDCDD533F98889C5D002604 | svc.efaktur.pajak.co.id </title>
+        <link type="image/x-icon" rel="shortcut icon" href="/web/image/website/1/favicon?unique=be92d55"/>
+        <link rel="preload" href="/web/static/lib/fontawesome/fonts/fontawesome-webfont.woff2?v=4.7.0" as="font" crossorigin=""/>
+        <link type="text/css" rel="stylesheet" href="/web/assets/352-c577ae3/1/web.assets_common.min.css" data-asset-bundle="web.assets_common" data-asset-version="c577ae3"/>
+        <link type="text/css" rel="stylesheet" href="/web/assets/353-c8ffbb7/1/web.assets_frontend.min.css" data-asset-bundle="web.assets_frontend" data-asset-version="c8ffbb7"/>
+        <script id="web.layout.odooscript" type="text/javascript">
+            var odoo = {
+                csrf_token: "c85bbbde00f6a5178c39ed0e4440a1f2e4d68d4eo1694769897",
+                debug: "",
+            };
+        </script>
+        <script type="text/javascript">
+            odoo.__session_info__ = {"is_admin": false, "is_system": false, "is_website_user": true, "user_id": false, "is_frontend": true, "profile_session": null, "profile_collectors": null, "profile_params": null, "show_effect": true, "translationURL": "/website/translations", "cache_hashes": {"translations": "d7beb64ac253949ed697973c3a3395dba064d656"}, "geoip_country_code": "ID"};
+            if (!/(^|;\s)tz=/.test(document.cookie)) {
+                const userTZ = Intl.DateTimeFormat().resolvedOptions().timeZone;
+                document.cookie = `tz=${userTZ}; path=/`;
+            }
+        </script>
+        <script defer="defer" type="text/javascript" src="/web/assets/188-c023bbd/1/web.assets_common_minimal.min.js" data-asset-bundle="web.assets_common_minimal" data-asset-version="c023bbd"></script>
+        <script defer="defer" type="text/javascript" src="/web/assets/354-5773987/1/web.assets_frontend_minimal.min.js" data-asset-bundle="web.assets_frontend_minimal" data-asset-version="5773987"></script>
+        <script defer="defer" type="text/javascript" data-src="/web/assets/355-df7c3b3/1/web.assets_common_lazy.min.js" data-asset-bundle="web.assets_common_lazy" data-asset-version="df7c3b3"></script>
+        <script defer="defer" type="text/javascript" data-src="/web/assets/356-bc9b0d3/1/web.assets_frontend_lazy.min.js" data-asset-bundle="web.assets_frontend_lazy" data-asset-version="bc9b0d3"></script>
+        
+        
+    </head>
+    <body class="">
+        <div id="wrapwrap" class="   ">
+                <main>
+                    
+    <div id="wrap" class="oe_structure oe_empty">
+      <section class="s_text_block o_colored_level pt0 pb0" data-snippet="s_text_block" data-name="Text" style="background-image: none;">
+        <div class="s_allow_columns o_container_preview container-fluid">
+          <p>
+            <span style="color: rgb(33, 37, 41); font-size: 9px">Kode dan Nomor Seri Faktur Pajak : 010.007-21.03081703,,,,,
+Pengusaha Kena Pajak,,,,,
+"Nama : CV MEGA UTAMA MOTOR
+Alamat : JL.KH MOCH MANSYUR NO.41 RT.005 RW.02 KEL DURI SELATAN , JAKARTA BARAT NPWP : 02.293.540.7-033.000,,,,,"
+Pembeli Barang Kena Pajak / Penerima Jasa Kena Pajak,,,,,
+"Nama : PT. ASSANE MAKMUR
+Alamat : JL KS TUBUN I GG HARLAND NO 2 A RT 005 RW 002 , KOTA BAMBU SELATAN PALMERAH JAKARTA BARAT 11420
+NPWP : 03.093.532.4-031.000,,,,,"
+No.,Nama Barang Kena Pajak / Jasa Kena Pajak,Harga Jual/Penggantian/Uang Muka/Termin,,,
+1,"ALL NEW AEROX 155 CONNECTED
+Rp 19.181.818 x 6","115,090,909.00",,,
+Harga Jual / Penggantian,,"115,090,909.00",,,
+Dikurangi Potongan Harga,,0.00,,,
+Dikurangi Uang Muka,,0.00,,,
+Dasar Pengenaan Pajak,,"115,090,909.00",,,
+PPN = 11% x Dasar Pengenaan Pajak,,"11,509,091.00",,,
+Total PPnBM (Pajak Penjualan Barang Mewah),,0.00,,,</span>
+            <br/>
+          </p>
+        </div>
+      </section>
+    </div>
+                </main>
+                <footer id="bottom" data-anchor="true" data-name="Footer" class="o_footer o_colored_level o_cc d-none o_snippet_invisible">
+                    <div id="footer" class="oe_structure oe_structure_solo">
+            
+        </div>
+                </footer>
+            </div>
+        
+        </body>
+</html>
